e64d1ec8768ddf6846bbd676d1b294c7f4bdb3d1
soumya ojha <soumyaojha685@gmail.com>
2026-06-08 22:26:47 +0530

    updated the app.py code . Addition of the users.db file

--- app.py
+++ app.py
@@ -1,10 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import sqlite3
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("users.db")
+        cursor = conn.cursor()
+
+        cursor.execute(
+            """
+            INSERT INTO users(username,email,password)
+            VALUES(?,?,?)
+            """,
+            (username, email, password)
+        )
+
+        conn.commit()
+        conn.close()
+
+        return "Registration Successful"
+
+    return render_template("register.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
